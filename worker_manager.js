e92ec6fdf14825541516c28878d0fdaba643b500
Einsten42 <milne.james@gmail.com>
2019-01-28 07:42:23 -0600

    fixed python catch

--- worker_manager.js
+++ worker_manager.js
@@ -91,7 +91,7 @@ async function createService(cmd, fullMsg) {
     })))
     let image = `pgc_nodeserver:`
     if (STAGE === 'test') { image += `beta_`}
-    data.language === 'python' ? image += 'python' : image += 'node'
+    data.language.toLowerCase().includes('python') ? image += 'python' : image += 'node'
     if (image === `pgc_nodeserver:`) { LOGGER.error(`createService: Bad Image: ${image}`, fullMsg.userId) }
     let PGURL=`${PARAMS.NS_DATA_URL}${params}`
     let service = await DOCKER.createService({
